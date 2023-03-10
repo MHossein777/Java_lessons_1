b717a87a77281fa15fe857a90f0db9f3617901b9
Heshmat <h.niafar@gmail.com>
2023-03-10 15:38:03 +0000

    Initial commit

---
+++ src/loop.java
@@ -0,0 +1,11 @@
+public class loop {
+    public static void main (String[] args){
+        int sum = 0;
+        for(int num =0 ; num <= 100; num++ )
+        {
+            sum+=num;
+
+        }
+        System.out.println(sum);
+    }
+}
